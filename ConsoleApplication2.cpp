62832f2cea0b6bb5de2b3e348998c02a022e1668
FelixVoro <115408780+FelixVoro@users.noreply.github.com>
2022-10-16 17:50:53 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	int b, c, e;
+	string a, d;
+	cout << "Input your name - ";
+	cin >> a;
+	cout << "\nInput your age - ";
+	cin >> b;
+	cout << "\nInput your weight - ";
+	cin >> c;
+	cout << "\nWhat color is your hair?  - ";
+	cin >> d;
+	cout << "\nInput ypur height - ";
+	cin >> e;
+	cout << "\nName:";
+	cout << a;
+	cout << "\nAge:";
+	cout << b;
+	cout << "\nWeight:";
+	cout << c;
+	cout << "\nHair Color:";
+	cout << d;
+	cout << "\nHeight:";
+	cout << e;
+}
